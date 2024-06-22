0a686bb2c278c26145734006b7b06574f72a6250
ClassicPawan <65965688+ClassicPawan@users.noreply.github.com>
2024-06-22 10:12:46 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = "30abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // will get NaN
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // will get NaN
 
 //"33" => 33
 //"33abs" => NaN
@@ -17,7 +17,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // "" => false
 // "pawan" => true
@@ -25,5 +25,37 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *********************** Opertaions **********************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " pawan"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
